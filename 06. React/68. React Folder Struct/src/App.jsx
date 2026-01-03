c1133571e1c61cdf5e7bdc30217d08494173da43
Pravin Mandal <pravinmandalp45@gmail.com>
2026-01-04 03:47:48 +0530

    React Folder Struct

---
+++ 06. React/68. React Folder Struct/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is the beggining of my journey</h1>
+      <h2>let's do some pro shits</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ 06. React/68. React Folder Struct/src/main.jsx
@@ -0,0 +1,6 @@
+import './index.css'
+import ReactDOM from 'react-dom/client'
+import app from './App'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(app());
