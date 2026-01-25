96d0572e04543768fc5da0ad71746908bfe1f21f
Pravin Mandal <pravinmandalp45@gmail.com>
2026-01-26 01:59:35 +0530

    Backend start

---
+++ 02. Backend/84. Origin/index.js
@@ -0,0 +1,6 @@
+// const catMe = require("cat-me");
+// console.log(catMe());
+
+const express = require("express")
+const app = express(); // server create kar chuke hai
+app.listen(3000) // server ko start krta hai
