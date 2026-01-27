08031840aa6a3e28e73aa8225a4d3e9f5d9af39e
Pravin Mandal <pravinmandalp45@gmail.com>
2026-01-27 13:39:57 +0530

    creting server

---
+++ 02. Backend/86. Programming Server/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+app.get("/", (req, res)=> {
+    res.send("Hello World")
+})
+app.get("/about", function(req, res) {
+    res.send("This is about page")
+})
+app.get("/home", (req, res)=> {
+    res.send("This is home page")
+})
+app.listen(3000);
