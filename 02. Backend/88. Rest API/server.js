fb9dfbb9989f712ce8329328993340d79607c1a4
Pravin Mandal <pravinmandalp45@gmail.com>
2026-01-31 11:52:28 +0530

    delete and patch revisited

--- 02. Backend/88. Rest API/server.js
+++ 02. Backend/88. Rest API/server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app")
 
 app.listen(3000, ()=> {
-    console.log("Server successfully hosted on port 3000")
+    console.log("Server started on port 3000")
 })
--- 02. Backend/88. Rest API/src/app.js
+++ 02. Backend/88. Rest API/src/app.js
@@ -6,7 +6,7 @@ const notes = []
 
 app.post("/notes", (req, res)=> {
     notes.push(req.body)
-    res.send("Note created successfully")
+    res.send("notes created successfully")
 })
 
 app.get("/notes", (req, res)=> {
@@ -15,12 +15,12 @@ app.get("/notes", (req, res)=> {
 
 app.delete("/notes/:index", (req, res)=> {
     delete notes[req.params.index]
-    res.send("Note deleted successfully")
+    res.send("note deleted successfully")
 })
 
 app.patch("/notes/:index", (req, res)=> {
     notes[req.params.index].description = req.body.description
-    res.send("Note updated successfully")
+    res.send("note patched successfully")
 })
 
-module.exports = app;
+module.exports = app
