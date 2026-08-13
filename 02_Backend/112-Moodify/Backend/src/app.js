a608d8721d73197f1197ed2c43d160488ea17286
Pravin Mandal <pravinmandalp45@gmail.com>
2026-08-14 01:23:03 +0530

    chore: update app.js for improved structure and organization

--- 02_Backend/112-Moodify/Backend/src/app.js
+++ 02_Backend/112-Moodify/Backend/src/app.js
@@ -1,7 +1,6 @@
 ﻿const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
-
 const app = express();
 
 app.use(express.json());
